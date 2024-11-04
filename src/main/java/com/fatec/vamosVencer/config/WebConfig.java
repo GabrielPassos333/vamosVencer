314f794db84e34dcb98060c0e3f24bbc842b233f
Gabriel Cruz dos Passos <passosc.gabriel@hotmail.com>
2024-11-04 11:44:19 -0300

    Docker

--- src/main/java/com/fatec/vamosVencer/config/WebConfig.java
+++ src/main/java/com/fatec/vamosVencer/config/WebConfig.java
@@ -10,27 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-  /*@Override
+  @Override
   public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**");
-  }*/
-
-  //public void addCorsMappings(CorsRegistry registry) {
-   // registry.addMapping("/**");
-      //.allowedOrigins("*")
-      //.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-  //}
-
-  @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600); // 1 hour
-    }
-
+  }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -43,4 +26,20 @@ public class WebConfig implements WebMvcConfigurer {
             }
         };
     }
+
+    /*public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+      .allowedOrigins("*")
+      .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }*/
+
+    /*@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600); // 1 hour
+    }*/
 }
