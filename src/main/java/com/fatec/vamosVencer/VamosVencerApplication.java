aa867cb3213dd2a4a5b5934a3c61da1dc18fb7f5
Gabriel Cruz dos Passos <passosc.gabriel@hotmail.com>
2024-11-05 17:20:01 -0300

    serverUpdates

--- src/main/java/com/fatec/vamosVencer/VamosVencerApplication.java
+++ src/main/java/com/fatec/vamosVencer/VamosVencerApplication.java
@@ -2,6 +2,7 @@ package com.fatec.vamosVencer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * Classe VamosVencerApplication
@@ -16,5 +17,11 @@ public class VamosVencerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VamosVencerApplication.class, args);
 	}
+
+	 // Método para imprimir mensagem no console a cada 13 minutos
+    @Scheduled(fixedRate = 780000) // 780000 ms = 13 minutos
+    public void keepAlive() {
+        System.out.println("O servidor está ativo - " + java.time.LocalTime.now());
+    }
 
 }
